39863436f7e2e88795c84b8a1afa7cb566b12998
gre1wy <grundayaroslav28@gmail.com>
2023-11-14 15:32:40 +0200

    almost completed

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,43 +1,56 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-#image = cv2.imread(r'images/.jpg')
-image = cv2.imread(r'images\.jpg', cv2.IMREAD_GRAYSCALE)
+import kernels as k
+import time
 
-# Define a 3x3 kernel (example: identity kernel)
-#kernel = np.array([[0, 0, 0],
-                  #[0, 0, 1],
-                  #[0, 0, 0]])
+start = time.time() ## точка отсчета времени
 
-kernel = (1 / 256.0) * np.array([[1, 4, 6, 4, 1],
-                                   [4, 16, 24, 16, 4],
-                                   [6, 24, 36, 24, 6],
-                                   [4, 16, 24, 16, 4],
-                                   [1, 4, 6, 4, 1]])
-# Get the dimensions of the image and kernel
-image_height, image_width = image.shape
-kernel_height, kernel_width = kernel.shape
+np.set_printoptions(threshold = np.inf)
+image = cv2.imread(r'images\Test.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('bw.jpg', image)
+kernel = k.Rizkist()
 
-# Create an output image (result) with the same dimensions
-result = np.zeros((image_height, image_width), dtype=np.uint8)
+#Тест на рандом np.array
+#np.random.seed(42)
+#random_array = np.random.randint(0, 256, size=(10, 10))
+#print('random_array')
+#print(random_array)
 
-# Perform convolution
-for y in range(image_height):
-    for x in range(image_width):
-        for ky in range(kernel_height):
-            for kx in range(kernel_width):
-                # Calculate the coordinates in the kernel
-                k_x = kernel_width - 1 - kx
-                k_y = kernel_height - 1 - ky
+def filter2D(image, kernel):
+    image_height, image_width = image.shape
+    kernel_height, kernel_width = kernel.shape
 
-                # Calculate the coordinates in the image
-                img_x = x - kernel_width // 2 + k_x
-                img_y = y - kernel_height // 2 + k_y
+    padded_image = np.pad(image, max(kernel_height//2, kernel_width//2), mode='constant', constant_values=0)
+    #print('padded_image')
+    #print(padded_image)
+    #print('kernel')
+    #print(kernel)
+    filtered_image = np.zeros_like(image, dtype=float)
+    #print('filtered_image')
+    #print(filtered_image)
 
-                # Check if the coordinates are within the image bounds
-                if 0 <= img_x < image_width and 0 <= img_y < image_height:
-                    result[y, x] += image[img_y, img_x] * kernel[k_y, k_x]
+    for i in range(image_height):
+        for j in range(image_width):
+            # patch это подстрока которая имеет такой же размер как и ядро,
+            # 'i: i + kernel_height' указывает диапазон строк для извлечения:
+            # начинается с текущего значения i и продолжается до i + kernel_height - 1.
+            # 'j: j + kernel_width' указывает диапазон столбцов для извлечения
+            # начинается с текущего значения j и продолжается до j + kernel_height - 1.
+            # например первый патч: [[   0.   -0.    0.]
+            #                        [  -0.  510. -179.]
+            #                        [   0. -210.    0.]]
+            patch = padded_image[i:i + kernel_height, j:j + kernel_width]
+            #print(patch)
+            filtered_pixel = np.sum(patch * kernel)
+            filtered_image[i, j] = filtered_pixel
 
-# Save the result
+    #print(filtered_image)
+    result_image = np.clip(filtered_image, 0, 255)
+    print(result_image)
+    return result_image
+result = filter2D(image, kernel)
 cv2.imwrite('convolved_image.jpg', result)
-print(result.shape)
+print('end')
+end = time.time() - start ## собственно время работы программы
+
+print(end) ## вывод времени
