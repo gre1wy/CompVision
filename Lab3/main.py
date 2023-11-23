b7201c36801174c8e11ce9b05d7da6da46e85f7c
gre1wy <grundayaroslav28@gmail.com>
2023-11-23 20:32:46 +0200

    completed

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
-np.set_printoptions(threshold = np.inf)
-name = "apple"
-image_read = cv2.imread(f'images/{name}.jpg')
+
 def bw_method(image):
     height, width, _ = image.shape
     image1 = np.zeros((height, width), dtype=np.uint8)
@@ -25,25 +23,76 @@ def binarization_on_bw_image(image):
             else:
                 image[y, x] = 0
     return image1
-def dilation(image, kernel, offset_y=0, offset_x=0):
-    image_height, image_width = image.shape
-    kernel_height, kernel_width = kernel.shape
 
+
+def process_special_point(matrix):
+    # Создаем копию матрицы
+    matrix_copy = matrix.copy()
+
+    # Найти координаты элемента, начинающегося на 'sp'
+    coordinates = np.argwhere(np.char.startswith(matrix_copy.astype(str), 'sp'))
+
+    if len(coordinates) == 0:
+        # print("Элементы, начинающиеся на 'sp', не найдены.")
+        return matrix_copy, [0, 0]
+
+    # Извлечь значения после 'sp', заменить в матрице
+    for coord in coordinates:
+        value = matrix_copy[coord[0], coord[1]][2:]  # Получаем значение после 'sp'
+        matrix_copy[coord[0], coord[1]] = value
+
+    # Преобразовать копию матрицы в int
+    matrix_copy = matrix_copy.astype(int)
+
+    # Найти центр матрицы
+    center = np.array(matrix_copy.shape) // 2
+
+    # Найти разницу между координатами и центром матрицы
+    differences = coordinates - center
+
+    return matrix_copy, differences[0].tolist()
+
+
+def dilation(image, kernel):
+    result_kernel, differences = process_special_point(kernel)
+    # print(differences)
+    offset_y = differences[0]
+    offset_x = differences[1]
+
+    result_kernel = result_kernel.astype(int) #не знаю почему без этого возвращает тип u32
+
+    image_height, image_width = image.shape
+    kernel_height, kernel_width = result_kernel.shape
+
+    # Создаем изображение как входное, только с нулями на границах взависимости от ядра
     padded_image = np.pad(image, max(kernel_height//2, kernel_width//2), mode='constant', constant_values=0)
+    # Создаем изображение размером как входное, только с нулями
     filtered_image = np.zeros_like(image, dtype=np.uint8)
-    where_white_pixel_kernel = np.where(kernel != 0)
+    # Находим координаты белых пикселей в ядре что бы потом сравнивать с белыми пикселями в патче
+    where_white_pixel_kernel = np.where(result_kernel != 0)
     check_kernel = list(zip(where_white_pixel_kernel[0], where_white_pixel_kernel[1]))
     # t = 0
 
     for i in range(image_height):
         for j in range(image_width):
+
+            if i + offset_y < 0 or j + offset_x < 0:
+                # print('Выходит за границу изображения')
+                continue
+
+            # Создаем патч из изображения для каждой координаты
             patch = padded_image[i:i + kernel_height, j:j + kernel_width]
-            # print(np.column_stack((patch, kernel)))
-            where_white_pixel_patch = np.where(kernel * patch != 0)
+
+            # print(np.column_stack((patch, result_kernel)))
+
+            # Находим координаты белых точек после прикладывания патча
+            where_white_pixel_patch = np.where(result_kernel * patch != 0)
             check_patch = list(zip(where_white_pixel_patch[0], where_white_pixel_patch[1]))
             try:
+                # Проверяем есть ли хоть один одинаковый белый пиксель
                 if any(elem in check_patch for elem in check_kernel):
                     filtered_image[i+offset_y, j+offset_x] = 255
+                    # print([i+offset_y, j+offset_x])
                     # print('hit')
                 else:
                     filtered_image[i+offset_y, j+offset_x] = 0
@@ -57,29 +106,49 @@ def dilation(image, kernel, offset_y=0, offset_x=0):
             # print(filtered_image)
     return filtered_image
 
-def erosian(image, kernel, offset_y=0, offset_x=0):
+
+def erosian(image, kernel):
+    result_kernel, differences = process_special_point(kernel)
+    offset_y = differences[0]
+    offset_x = differences[1]
+    # print(differences)
+    result_kernel = result_kernel.astype(int)
+
     image_height, image_width = image.shape
-    kernel_height, kernel_width = kernel.shape
+    kernel_height, kernel_width = result_kernel.shape
 
     padded_image = np.pad(image, max(kernel_height//2, kernel_width//2), mode='constant', constant_values=0)
     filtered_image = np.zeros_like(image, dtype=np.uint8)
-    where_white_pixel_kernel = np.where(kernel != 0)
+    where_white_pixel_kernel = np.where(result_kernel != 0)
     check_kernel = list(zip(where_white_pixel_kernel[0], where_white_pixel_kernel[1]))
+
+    # t = 0
+
     for i in range(image_height):
         for j in range(image_width):
+            if i + offset_y < 0 or j + offset_x < 0:
+                # print('Выходит за границу изображения')
+                continue
             patch = padded_image[i:i + kernel_height, j:j + kernel_width]
-            where_white_pixel_patch = np.where(kernel * patch != 0)
+            where_white_pixel_patch = np.where(result_kernel * patch != 0)
             check_patch = list(zip(where_white_pixel_patch[0], where_white_pixel_patch[1]))
-            #print(check_kernel, check_patch)
+            # print(np.column_stack((patch, result_kernel)))
             try:
                 if all(elem in check_patch for elem in check_kernel):
                     filtered_image[i+offset_y, j+offset_x] = 255
+                    # print([i+offset_y, j+offset_x])
+                    # print('fit')
                 else:
                     filtered_image[i+offset_y, j+offset_x] = 0
+                    # print([i + offset_y, j + offset_x])
+                    # print('miss')
             except Exception as e:
                 print(f"Произошла ошибка: {e}")
                 filtered_image[i, j] = 0
                 pass
+            # t +=1
+            # print(f"image {t}")
+            # print(filtered_image)
 
     return filtered_image
 def closing(image, kernel):
@@ -93,43 +162,3 @@ def opening(image, kernel):
 def limits(image, kernel):
     return image ^ erosian(image, kernel)
 
-# test_image = np.array([[0,255,0,0,255,0],
-#                        [255,255,255,0,255,0],
-#                        [0,255,0,0,255,255],
-#                        [0,255,255,0,255,255],
-#                        [255,0,255,0,0,255],
-#                        [0,0,0,0,255,0]])
-# test_kernel = np.array([[0,255,0],
-#                         [0,255,0],
-#                         [0,255,0]])
-#
-# test1 = dilation(test_image, test_kernel)
-# test2 = erosian(test_image, test_kernel)
-# test3 = closing(test_image, test_kernel)
-# test4 = opening(test_image, test_kernel)
-# test5 = limits(test_image, test_kernel)
-# cv2.imwrite(f'Images_output/{"0original_image_"+"test_image"}.jpg', test_image)
-# cv2.imwrite(f'Images_output/{"1dilation_image_"+"test_image"}.jpg', test1)
-# cv2.imwrite(f'Images_output/{"2erosion_image_"+"test_image"}.jpg', test2)
-# cv2.imwrite(f'Images_output/{"3closing_image_"+"test_image"}.jpg', test3)
-# cv2.imwrite(f'Images_output/{"4opening_image_"+"test_image"}.jpg', test4)
-# cv2.imwrite(f'Images_output/{"5limits_image_"+"test_image"}.jpg', test5)
-
-
-image1 = bw_method(image_read)
-image = binarization_on_bw_image(image1)
-kernel = np.array([[0,255,0],
-                   [0,255,0],
-                   [0,255,0]])
-dilation1 = dilation(image, kernel)
-erosion2 = erosian(image, kernel)
-closing3 = closing(image, kernel)
-opening4 = opening(image, kernel)
-limits5 = limits(image, kernel)
-
-cv2.imwrite(f'Images_output/{"0original_image_"+name}.jpg', image)
-cv2.imwrite(f'Images_output/{"1dilation_image_"+name}.jpg', dilation1)
-cv2.imwrite(f'Images_output/{"2erosion_image_"+name}.jpg', erosion2)
-cv2.imwrite(f'Images_output/{"3closing_image_"+name}.jpg', closing3)
-cv2.imwrite(f'Images_output/{"4opening_image_"+name}.jpg', opening4)
-cv2.imwrite(f'Images_output/{"5limits_image_"+name}.jpg', limits5)
---
+++ Lab3/test.py
@@ -0,0 +1,58 @@
+import main as m
+import numpy as np
+import cv2
+np.set_printoptions(threshold = np.inf)
+
+
+
+"""Обработка тестовой картинки"""
+
+# test_image = np.array([[0,255,0,0,255,0],
+#                        [255,255,255,0,255,0],
+#                        [0,255,0,0,255,255],
+#                        [0,255,255,0,255,255],
+#                        [255,0,255,0,0,255],
+#                        [0,0,0,0,255,0]])
+# test_kernel = np.array([[0,255,0],
+#                         ['sp0',255,0],
+#                         [0,255,0]])
+#
+#
+# test1 = m.dilation(test_image, test_kernel)
+# test2 = m.erosian(test_image, test_kernel)
+# test3 = m.closing(test_image, test_kernel)
+# test4 = m.opening(test_image, test_kernel)
+# test5 = m.limits(test_image, test_kernel)
+# cv2.imwrite(f'Images_output/{"0original_image_"+"test_image"}.jpg', test_image)
+# cv2.imwrite(f'Images_output/{"1dilation_image_"+"test_image"}.jpg', test1)
+# cv2.imwrite(f'Images_output/{"2erosion_image_"+"test_image"}.jpg', test2)
+# cv2.imwrite(f'Images_output/{"3closing_image_"+"test_image"}.jpg', test3)
+# cv2.imwrite(f'Images_output/{"4opening_image_"+"test_image"}.jpg', test4)
+# cv2.imwrite(f'Images_output/{"5limits_image_"+"test_image"}.jpg', test5)
+
+"""Обработка изображения"""
+
+name = "apple"
+# name = 'Text'
+image_read = cv2.imread(f'images/{name}.jpg')
+
+image1 = m.bw_method(image_read)
+image = m.binarization_on_bw_image(image1)
+
+kernel = np.array([[0,255,0],
+                   [0,255,0],
+                   [0,255,0]])
+
+dilation1 = m.dilation(image, kernel)
+erosion2 = m.erosian(image, kernel)
+closing3 = m.closing(image, kernel)
+opening4 = m.opening(image, kernel)
+limits5 = m.limits(image, kernel)
+
+cv2.imwrite(f'Images_output/{"0original_image_"+name}.jpg', image)
+cv2.imwrite(f'Images_output/{"1dilation_image_"+name}.jpg', dilation1)
+cv2.imwrite(f'Images_output/{"2erosion_image_"+name}.jpg', erosion2)
+cv2.imwrite(f'Images_output/{"3closing_image_"+name}.jpg', closing3)
+cv2.imwrite(f'Images_output/{"4opening_image_"+name}.jpg', opening4)
+cv2.imwrite(f'Images_output/{"5limits_image_"+name}.jpg', limits5)
+
